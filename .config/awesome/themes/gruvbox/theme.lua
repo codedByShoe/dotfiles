6785885c8e18db014428b117c59b54b6716f5e19
codedbyshoe <andrew.shoemaker9@gmail.com>
2025-07-11 02:22:52 -0500

    changes

---
+++ .config/awesome/themes/gruvbox/theme.lua
@@ -0,0 +1,32 @@
+local theme               = {}
+
+-- Gruvbox Dark Soft
+theme.bg_normal           = "#282828"
+theme.bg_focus            = "#3c3836"
+theme.bg_urgent           = "#cc241d"
+theme.bg_minimize         = "#3c3836"
+theme.bg_systray          = theme.bg_normal
+
+theme.fg_normal           = "#ebdbb2"
+theme.fg_focus            = "#fabd2f"
+theme.fg_urgent           = "#fbf1c7"
+theme.fg_minimize         = "#a89984"
+
+theme.border_width        = 2
+theme.border_normal       = "#1d2021"
+theme.border_focus        = "#d65d0e"
+theme.border_marked       = "#458588"
+
+-- Taglist
+theme.taglist_bg_focus    = "#00000000"
+theme.taglist_fg_focus    = "#fabd2f"
+-- theme.taglist_bg_occupied = "#3c3836"
+theme.taglist_fg_occupied = "#d79921"
+-- theme.taglist_bg_empty    = "#282828"
+-- theme.taglist_fg_empty    = "#928374"
+
+-- Fonts
+theme.font                = "JetBrainsMono Nerd Font 12"
+theme.taglist_font        = "JetBrainsMono Nerd Font Bold 10"
+
+return theme
